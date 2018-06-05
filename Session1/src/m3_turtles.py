ecaa6fb04c32ee4c63ab50f1bcd77fa86920bf4e
nowakoea <nowakoea@rose-hulman.edu>
2018-06-05 00:45:58 -0400

    Session 1

--- Session1/src/m3_turtles.py
+++ Session1/src/m3_turtles.py
@@ -126,7 +126,7 @@ nick.backward(200)
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
