fce4974b0695262f14c72dba963798868c53c3dc
nowakoea <nowakoea@rose-hulman.edu>
2018-06-05 00:41:45 -0400

    Session 1

--- Session1/src/m3_turtles.py
+++ Session1/src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Elle Nowakowski.
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -63,6 +63,8 @@ dave = rg.SimpleTurtle()
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
+dave.left(90)
+dave.forward(100)
 
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
@@ -73,12 +75,13 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
-
+matt.right(45)
+matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
-#   Add a few more line of your own code above to make one of the
+# DONE: 3.
+#   Add a few more lines of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
 #
@@ -91,7 +94,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,6 +112,17 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+
+# ----------------------------------------------------------------------
+# Construct a new turtle and ask it to do things.
+# ----------------------------------------------------------------------
+nick = rg.SimpleTurtle('turtle')
+nick.pen = rg.Pen('blue', 30)
+nick.speed = 7
+nick.right(100)
+nick.forward(300)
+nick.left(70)
+nick.backward(200)
 
 ########################################################################
 #
