83f800a9d1b634c78730cc405d9a64734af6b583
nowakoea <nowakoea@rose-hulman.edu>
2018-06-18 19:04:03 -0400

    Quizzes

--- Quizzes.py
+++ Quizzes.py
@@ -1,40 +1,54 @@
 # Quiz 3
 
-def blah(x):
-    print(x * x)
+# def blah(x):
+#     print(x * x)
+#
+#
+# print(blah(5))
+#
+# size = 10
+# for k in range(3):
+#     size = size + 5
+# size = size - 2
+# print(size)
+#
+#
+# def mystery(a, b):
+#     c = a * b + a
+#     return c
+#
+#
+# mystery(4, 5)
+# print('-----')
+# print(mystery(4, 5))
+# print('-----')
+# print(mystery(7, 10))
+#
+#
+# def mystery1(x, y):
+#     x = 5
+#     c = x * y
+#     return c
+#
+#
+# print(mystery1(8, 10))
+# print('-----')
+# x = 8
+# y = 10
+# print(mystery1(x, y))
+# print('-----')
+# print(x, y)
 
 
-print(blah(5))
+# Quiz 4
 
-size = 10
-for k in range(3):
-    size = size + 5
-size = size - 2
-print(size)
-
-
-def mystery(a, b):
-    c = a * b + a
-    return c
+def countEvens(n):
+    count = 0
+    for k in range(n):
+        if k % 2 == 0:
+            count = count + 1
+        print(k, count)
+    return count
 
 
-mystery(4, 5)
-print('-----')
-print(mystery(4, 5))
-print('-----')
-print(mystery(7, 10))
-
-
-def mystery1(x, y):
-    x = 5
-    c = x * y
-    return c
-
-
-print(mystery1(8, 10))
-print('-----')
-x = 8
-y = 10
-print(mystery1(x, y))
-print('-----')
-print(x, y)
+print(countEvens(4))
